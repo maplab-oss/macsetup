b538faa91abcd668878d5439427e5df177c6f1f8
Felix Sebastian <hi@felixsebastian.dev>
2025-05-14 14:11:45 +1000

    wip

--- start.sh
+++ start.sh
@@ -10,31 +10,28 @@ mkdir -p $HOME/Code
 # intstall homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 BREW=/opt/homebrew/bin
+export PATH="$BREW:$PATH"
 
 # clone this repo
-$BREW/brew install gh
-$BREW/gh auth login
-$BREW/gh repo clone felixsebastian/macsetup $HOME/Code/macsetup
+brew install gh
+gh auth login
+gh repo clone felixsebastian/macsetup $HOME/Code/macsetup
 
 # install oh my zsh
-KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # copy files
 cp $HOME/Code/macsetup/payloads/gitconfig $HOME/.gitconfig
 cp $HOME/Code/macsetup/payloads/zshrc $HOME/.zshrc
 cp $HOME/Code/macsetup/payloads/sandbox.sh $HOME/sandbox.sh
 
-source $HOME/.zshrc
-
 # install homebrew packages
-$BREW/brew install go-task/tap/go-task
-$BREW/brew install go-task node@20
-$BREW/brew install pipx
-$BREW/brew install pyenv
+brew install go-task/tap/go-task
+brew install go-task node@20 pipx pyenv awscli
 
-$BREW/gh auth login
-$BREW/pipx ensurepath
-$BREW/pipx install awscli
-$BREW/pyenv install 3.11
-$BREW/pipx install --python python3.11 aider-chat
-$BREW/pipx inject aider-chat google-generativeai
+gh auth login
+pipx ensurepath
+pipx install poetry
+
+npm install --global npm
+npm install --global pnpm yarn pm2
