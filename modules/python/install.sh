60df172b0c15b8956a6038611d332f0bd1224baa
Felix Sebastian <hi@felixsebastian.dev>
2025-06-30 08:04:08 +1000

    wip

--- modules/python/install.sh
+++ modules/python/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-brew install pipx pyenv;
-pipx ensurepath;
-pipx install poetry;
-poetry config virtualenvs.in-project true;
+brew install pipx pyenv
+export PATH="$HOME/.local/bin:$PATH"
+pipx install poetry
+poetry config virtualenvs.in-project true
