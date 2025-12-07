8ddf2e7f023ed9685559f3cfc4e01062308fc595
Felix Sebastian <hi@felixsebastian.dev>
2025-12-07 19:59:20 +1100

    wip

--- start-admin.sh
+++ start-admin.sh
@@ -9,17 +9,12 @@ command -v brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.github
 BREW=/opt/homebrew/bin
 export PATH="$BREW:$PATH"
 
-# clone this repo (needed for module scripts)
-brew install gh
-gh auth status >/dev/null 2>&1 || gh auth login
-[ -d "$HOME/Code/macsetup" ] || (mkdir -p $HOME/Code && gh repo clone felixsebastian/macsetup $HOME/Code/macsetup)
-
 # install homebrew packages
-brew install go-task/tap/go-task awscli tmux overmind
+brew install gh go-task/tap/go-task awscli tmux overmind
 
 # run module admin installers
-$HOME/Code/macsetup/modules/python/install-admin.sh
-$HOME/Code/macsetup/modules/ruby/install-admin.sh
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/felixsebastian/macsetup/main/modules/python/install-admin.sh)"
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/felixsebastian/macsetup/main/modules/ruby/install-admin.sh)"
 
 echo "✓ System-wide dependencies installed"
 echo ""
