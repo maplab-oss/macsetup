4dafeb30eb253172b0f57887e2a93fac98306a7b
Felix Sebastian <hi@felixsebastian.dev>
2025-12-07 20:04:55 +1100

    wip

--- modules/python/uninstall.sh
+++ modules/python/uninstall.sh
@@ -3,4 +3,7 @@
 export PATH="$HOME/.local/bin:$PATH"
 pipx uninstall poetry 2>/dev/null || true
 rm -rf $HOME/.pyenv
+rm -rf $HOME/.local
+rm -rf $HOME/.cargo
+rm -rf $HOME/.config/pypoetry
 
--- uninstall.sh
+++ uninstall.sh
@@ -2,9 +2,26 @@
 
 set -e
 
+# run module uninstallers
+[ -f "$HOME/Code/macsetup/modules/node/uninstall.sh" ] && $HOME/Code/macsetup/modules/node/uninstall.sh
+[ -f "$HOME/Code/macsetup/modules/python/uninstall.sh" ] && $HOME/Code/macsetup/modules/python/uninstall.sh
+[ -f "$HOME/Code/macsetup/modules/ruby/uninstall.sh" ] && $HOME/Code/macsetup/modules/ruby/uninstall.sh
+
+# remove core files
 rm -rf $HOME/Code
+rm -f $HOME/.gitconfig
+rm -f $HOME/.zshrc
+rm -f $HOME/sandbox.sh
+rm -f $HOME/.zsh_history
+rm -rf $HOME/.zsh_sessions
+rm -rf $HOME/.config/gh
+
+# uninstall oh my zsh
+source $HOME/.oh-my-zsh/tools/uninstall.sh
+
+# uninstall homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
 sudo rm -rf /opt/homebrew
-source $HOME/.oh-my-zsh/tools/uninstall.sh
-rm $HOME/.gitconfig
-rm $HOME/.zshrc
+
+echo "Note: Cache directories (~/.cache, ~/.npm, ~/.gem) and SSH keys (~/.ssh) may exist"
+echo "Review and manually delete them if needed"
