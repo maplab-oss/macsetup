9a2e0dcdd39c31f9fcfa2b1080e23cb492f1bb37
Felix Sebastian <hi@felixsebastian.dev>
2026-02-01 18:07:36 +1100

    wip

--- start-admin.sh
+++ start-admin.sh
@@ -8,7 +8,7 @@ log "Installing system-wide dependencies."
 command -v brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 BREW=/opt/homebrew/bin
 export PATH="$BREW:$PATH"
-brew install gh go-task/tap/go-task awscli tmux overmind cursor
+brew install gh go-task/tap/go-task awscli tmux overmind
 
 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/felixsebastian/macsetup/main/modules/node/install-admin.sh)"
 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/felixsebastian/macsetup/main/modules/python/install-admin.sh)"
